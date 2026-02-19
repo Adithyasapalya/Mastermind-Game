fa58437223a2694c3ee742792d0d1660acbeb1e2
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-02-19 22:49:15 +0530

    checking code

--- mmg.py
+++ mmg.py
@@ -31,3 +31,14 @@ def guess_code():
             break
     return guess
 
+def check_code(guess, real_code):
+    color_counts = {}
+    correct_pos = 0
+    incorrect_pos = 0
+
+    for color in real_code:
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] += 1
+
+    for guess_color, real_color in zip(guess, real_code)
